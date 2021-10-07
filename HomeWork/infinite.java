2372a3ae62e5e88b2ff6b17b3bd93a01f3097998
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-10-07 12:28:55 +0530

    Add files via upload

---
+++ HomeWork/infinite.java
@@ -0,0 +1,8 @@
+public class infinite {
+    public static void main(String[] args){
+        
+        do{
+            System.out.println("*");
+        }while(true);          /// this is infinite loop if we put another = ;
+    }
+}
