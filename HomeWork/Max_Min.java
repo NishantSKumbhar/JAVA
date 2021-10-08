6ad95d918d1f9560692d3982f3d8376ff2146131
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-10-08 12:01:07 +0530

    Add files via upload

---
+++ HomeWork/Max_Min.java
@@ -0,0 +1,14 @@
+public class Max_Min {
+    public static void main(String[] args) {
+        //Integer. MAX_VALUE represents the maximum positive integer value
+        // that can be represented in 32 bits (i.e., 2147483647 ). 
+        //This means that no number of type Integer that is greater than 2147483647 can exist in Java.
+
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        // this two numbers are greater than any other numbre in code;
+        if(max > 888){
+            System.out.println("Yes");
+        }
+    }
+}
