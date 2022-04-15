842d8ef0f37f3b2fe1f096a5197bb823e48585f2
Nishant <nish.kumbhar02@gmail.com>
2022-04-15 19:25:57 +0530

    Update ReverseStarPyramid.java

--- Java/ReverseStarPyramid.java
+++ Java/ReverseStarPyramid.java
@@ -6,16 +6,20 @@ class ReverseStarPyramid{
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
+		int flag = n;
+		for(int i = 1; i <= n; i++){
 
-		for(int i = n; i >= 1; i--){
-			for(int j = 1; j >= (n - i); j++){
+			for(int j = 1; j <= (i-1); j++){
 				System.out.print("   ");
 			}
-			for(int j = ((i * 2) - 1); j >= 1 ; j--){
+
+			for(int j = 1; j <= (n + (flag - i)); j++){
 				System.out.print(" * ");
 			}
+			flag--;
 			System.out.println();
 		}
+		
 	}
 }
 
